e8c1d872f7affcf821f8720ec4d8e875176f62fb
Hannes Erik Rother <hrother@c4r5p5.42vienna.com>
2023-09-14 17:34:21 +0200

    small start

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,24 @@
 /*   By: hrother <hrother@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 15:38:56 by hrother           #+#    #+#             */
-/*   Updated: 2023/09/14 15:41:44 by hrother          ###   ########.fr       */
+/*   Updated: 2023/09/14 16:24:24 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_printf(const char *str, ...)
 {
-	return (0);
+	va_list	args;
+	int		c_count;
+
+	va_start(args, str);
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		c_count++;
+	}
+	return (c_count);
 }
